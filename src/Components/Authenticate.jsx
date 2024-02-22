9c090c502e211284284ca2e501565e7791512ad0
JamalOthman1 <156115540+JamalOthman1@users.noreply.github.com>
2024-02-22 15:56:29 -0500

    Update Authenticate.jsx

--- src/Components/Authenticate.jsx
+++ src/Components/Authenticate.jsx
@@ -1,3 +1,32 @@
-export default function Authenticate() {
-  return <h2>Authenticate</h2>;
+import { useState } from "react";
+const Authenticate = ({ token }) => {
+  const [successMessage, setSuccessMessage] = useState(null);
+  const [error, setError] = useState(null);
+  async function handleClick() {
+    try {
+      const response = await fetch('https://fsa-jwt-practice.herokuapp.com/authenticate',
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${token}`
+          }
+        })
+
+      const json = await response.json()
+      setSuccessMessage(json.message)
+    } catch (error) {
+      setError(error.message)
+    }
+  }
+  return (
+    <>
+      <h1>Authenticate</h1>
+      {successMessage && <p>{successMessage}</p>}
+      {error && <p>{error}</p>}
+      <button onClick={handleClick}>Authorize!</button>
+    </>
+  )
 }
+
+export default Authenticate
